e1d8f137b10e760e58456b04dfd3c94f90a3473d
Michal Nebes <michal.nebes@digitecgalaxus.ch>
2018-12-04 09:33:58 +0100

    make solution to day1_2 nicer

--- Day1/day1_2a.fsx
+++ Day1/day1_2a.fsx
@@ -5,22 +5,18 @@ let getNumber (charList : char list) =
 let getOperation inputString =
     match Seq.toList inputString with
     | sign::number when sign = '+' -> (+) (getNumber number)
-    | sign::number when sign = '-' -> (+) -(getNumber number)
+    | sign::number when sign = '-' -> (+) (-(getNumber number))
 
-let findRepeatingSequence operations = 
-    let rec checkSequence state remainingOperations allOperations (sequences : Set<int>) =
+let findRepeatingSequence operations =
+    let rec checkSequence state remainingOperations (sequences : Set<int>) =
         if sequences.Contains state then state
-        else 
-            let newState = List.head remainingOperations <| state
+        else
+            let operation = List.head remainingOperations
             let newSequences = Set.add state sequences
-            let newRemainingOperations = 
-                match List.tail remainingOperations with
-                | [x] -> x::allOperations
-                | x::xs -> x::xs
-            checkSequence newState newRemainingOperations allOperations newSequences
-    checkSequence 0 operations operations Set.empty
-    
-let result = 
+            checkSequence (operation state) (List.append (List.tail remainingOperations) [ operation ]) newSequences
+    checkSequence 0 operations Set.empty
+
+let result =
     System.IO.File.ReadLines "Day1/input.txt"
         |> Seq.map getOperation
         |> List.ofSeq
